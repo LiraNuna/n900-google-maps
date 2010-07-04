ef6d070dd1b40a2880b3aeabb22ab5bbdf4f136f
Liran Nuna <liranuna@gmail.com>
2010-07-03 23:37:20 -0700

    Added missing texture coordinates. Project still does not compile on N900

--- source/googlemapwidget.cpp
+++ source/googlemapwidget.cpp
@@ -1,10 +1,17 @@
 #include "googlemapwidget.h"
 
 const GLint tileVertexies[] = {
-	  0, 255,   0,
-	255, 255,   0,
-	  0,   0,   0,
-	255,   0,   0,
+	  0, 256,
+	256, 256,
+	  0,   0,
+	256,   0,
+};
+
+const GLint tileTexCoords[] = {
+	0, 0,
+	1, 0,
+	0, 1,
+	1, 1,
 };
 
 const GLfloat identityMatrix[] = {
@@ -29,7 +36,9 @@ void GoogleMapWidget::initializeGL()
 	glClearColor(0.0, 0.0, 0.0, 0.0);
 
 	glEnableClientState(GL_VERTEX_ARRAY);
-	glVertexPointer(3, GL_INT, 0, tileVertexies);
+	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+	glVertexPointer(2, GL_INT, 0, tileVertexies);
+	glTexCoordPointer(2, GL_INT, 0, tileTexCoords);
 }
 
 void GoogleMapWidget::resizeGL(int width, int height)
@@ -52,7 +61,6 @@ void GoogleMapWidget::paintGL()
 		bindTexture(tile);
 
 	glTranslatef(translation.x(), translation.y(), 0);
-
 	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 }
 
