1209050e3b029305d5cfbb87e6c50a946045d7d6
Liran Nuna <liranuna@gmail.com>
2010-07-20 02:06:12 -0700

    Removal of debug; Move of the MVP matrix to the shader

--- source/googlemapwidget.cpp
+++ source/googlemapwidget.cpp
@@ -25,10 +25,16 @@ void GoogleMapWidget::initializeGL()
 		attribute vec4	position;\
 		attribute vec4	texture;\
 		\
-		uniform mat4	modelViewProjectionMatrix;\
 		uniform vec4	translation;\
 		\
 		varying vec2	texCoords;\
+		\
+		const mat4 modelViewProjectionMatrix = mat4(\
+			2.0/(800.0 - 0.0), 0.0, 0.0, -(800.0 + 0.0)/(800.0 - 0.0),\
+			0.0, 2.0/(0.0 - 480.0), 0.0, -(0.0 + 480.0)/(0.0 - 480.0),\
+			0.0, 0.0, -2.0/(1.0 - 0.0), -(1.0 + 0.0)/(1.0 - 0.0),\
+			0.0, 0.0, 0.0, 1.0\
+		);\
 		\
 		void main(void)\
 		{\
@@ -40,38 +46,9 @@ void GoogleMapWidget::initializeGL()
 	glShaderSource(fragShaderId, 1, &fragShaderSrc, NULL);
 	glCompileShader(fragShaderId);
 
-//v-------------------------------v//
-	GLint bShaderCompiled;
-	glGetShaderiv(fragShaderId, GL_COMPILE_STATUS, &bShaderCompiled);
-
-	if(!bShaderCompiled) {
-		int i32InfoLogLength, i32CharsWritten;
-		glGetShaderiv(fragShaderId, GL_INFO_LOG_LENGTH, &i32InfoLogLength);
-
-		char* pszInfoLog = new char[i32InfoLogLength];
-		glGetShaderInfoLog(fragShaderId, i32InfoLogLength, &i32CharsWritten, pszInfoLog);
-
-		printf("Failed to compile fragment shader: %s\n", pszInfoLog);
-		delete [] pszInfoLog;
-	}
-//^-------------------------------^//
-
 	GLuint vertShaderId = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertShaderId, 1, &vertShaderSrc, NULL);
 	glCompileShader(vertShaderId);
-
-//v-------------------------------v//
-    glGetShaderiv(vertShaderId, GL_COMPILE_STATUS, &bShaderCompiled);
-	if(!bShaderCompiled)
-	{
-		int i32InfoLogLength, i32CharsWritten;
-		glGetShaderiv(vertShaderId, GL_INFO_LOG_LENGTH, &i32InfoLogLength);
-		char* pszInfoLog = new char[i32InfoLogLength];
-        glGetShaderInfoLog(vertShaderId, i32InfoLogLength, &i32CharsWritten, pszInfoLog);
-		printf("Failed to compile vertex shader: %s\n", pszInfoLog);
-		delete [] pszInfoLog;
-	}
-//^-------------------------------^//
 
 	shaderProgram = glCreateProgram();
 	glAttachShader(shaderProgram, fragShaderId);
@@ -79,20 +56,6 @@ void GoogleMapWidget::initializeGL()
 	glBindAttribLocation(shaderProgram, 0, "position");
     glBindAttribLocation(shaderProgram, 1, "texture");
 	glLinkProgram(shaderProgram);
-
-//v-------------------------------v//
-	GLint bLinked;
-	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &bLinked);
-	if (!bLinked)
-	{
-		int ui32InfoLogLength, ui32CharsWritten;
-		glGetProgramiv(shaderProgram, GL_INFO_LOG_LENGTH, &ui32InfoLogLength);
-		char* pszInfoLog = new char[ui32InfoLogLength];
-		glGetProgramInfoLog(shaderProgram, ui32InfoLogLength, &ui32CharsWritten, pszInfoLog);
-		printf("Failed to link program: %s\n", pszInfoLog);
-		delete [] pszInfoLog;
-	}
-//^-------------------------------^//
 
 	glUseProgram(shaderProgram);
 
@@ -119,14 +82,9 @@ void GoogleMapWidget::initializeGL()
 	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(GLuint), (void*)(2 * sizeof(GLuint)));
 
 	textureId = bindTexture(tile);
-}
 
-float pfIdentity[] = {
-	2.0/(800 - 0), 0, 0, -(800 + 0)/(800 - 0),
-	0, 2.0/(0 - 480), 0, -(0 + 480)/(0 - 480),
-	0, 0, -2.0/(1 - 0), -(1 + 0)/(1 - 0),
-	0, 0, 0, 1,
-};
+	glUniform4f(glGetUniformLocation(shaderProgram, "translation"), 0, 0, 0, 0);
+}
 
 void GoogleMapWidget::paintGL()
 {
@@ -134,9 +92,6 @@ void GoogleMapWidget::paintGL()
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	glBindTexture(GL_TEXTURE_2D, textureId);
-	glUniform4f(glGetUniformLocation(shaderProgram, "translation"), translation.x(), translation.y(), 0, 0);
-	glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "modelViewProjectionMatrix"), 1, GL_FALSE, pfIdentity);
-
 	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 }
 
@@ -150,6 +105,7 @@ void GoogleMapWidget::mouseMoveEvent(QMouseEvent* event)
 	translation += event->pos() - dragStart;
 	dragStart = event->pos();
 
+	glUniform4f(glGetUniformLocation(shaderProgram, "translation"), translation.x(), translation.y(), 0, 0);
+
 	glDraw();
 }
-
